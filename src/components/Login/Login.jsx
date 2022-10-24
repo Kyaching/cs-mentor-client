4d55814b9fd577ec3bfa39da4cbc614b41b5d46b
kyaching <kyochingmarma@gmail.com>
2022-10-25 00:59:07 +0600

    added login and register components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -34,9 +34,7 @@ const Login = () => {
             className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
           />
           <div className="flex justify-end text-xs dark:text-gray-400">
-            <a rel="noopener noreferrer" href="#">
-              Forgot Password?
-            </a>
+            <Link>Forgot Password?</Link>
           </div>
         </div>
         <button className="block w-full p-3 text-center rounded-sm dark:text-gray-900 dark:bg-violet-400">
