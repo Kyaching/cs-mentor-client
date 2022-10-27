214ac77593ac57b0d31f7d2c0b157a85673ea3e3
kyaching <kyochingmarma@gmail.com>
2022-10-27 20:53:51 +0600

    update checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -6,13 +6,13 @@ const Checkout = () => {
   const { user } = useContext(AuthContext);
   const course = useLoaderData();
   return (
-    <div className="flex justify-center items-center h-96 flex-col">
-      <h1>
-        <span className="text-orange-500 text-4xl">
+    <div className="flex justify-center  items-center h-96 flex-col">
+      <h1 className="text-center">
+        <span className="text-orange-500 font-semibold text-xl md:text-4xl">
           Congratulations!! {user?.displayName}
         </span>
       </h1>
-      <div className="text-center text-3xl md:w-1/2 font-bold mt-5">
+      <div className="text-center text-xl md:text-3xl md:w-1/2 font-bold mt-5">
         <h3>
           You get the premium access
           <span className="text-info"> For</span>
